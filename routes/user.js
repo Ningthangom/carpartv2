4d4b3893b251eacfb4da157c1b34e57b005685c5
Ningthangom <ningthangom@gmail.com>
2021-02-02 09:43:37 +1030

    .findOne is not working

--- routes/user.js
+++ routes/user.js
@@ -1,7 +1,6 @@
 const express = require('express');
 
 const router = express.Router();
-
 const mongoose = require('mongoose')
 const requiredLogin = require('../middleware/requireLogin')
 const Post = mongoose.model("Post")
@@ -9,13 +8,16 @@ const User = mongoose.model("User")
 
 // id is a parameter
 router.get('/user/:id',requiredLogin,(req,res)=>{
-    console.log("this is from backed ",id)
+    console.log("this is from backed ",req.params.id)
     //find the user and it's detail
     User.findOne({_id:req.params.id})
+    console.log({_id:req.params.id})
+    console.log(req.params.id)
     //selected ones here will not be shown in the front end
     // do not select password
     .select("-password")
     .then(user => {
+        console.log(user)
         Post.find({postedBy:req.params.id})
         .populate("postedBy","_id name")
         .exec((err,posts)=>{
